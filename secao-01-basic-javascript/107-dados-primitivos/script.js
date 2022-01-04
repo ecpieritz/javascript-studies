719950065274d4bc18175bc8bb4b1fd012f6d3ee
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-01-04 11:14:30 -0300

    added lesson 107

---
+++ secao-01-basic-javascript/107-dados-primitivos/script.js
@@ -0,0 +1,18 @@
+// string
+const nome1 = 'Emilyn'
+
+// number
+const num1 = 30
+const num2 = 25.456
+
+// undefined = não aponta pra local nenhum na memória
+let nomeAluno
+
+// null = não aponta pra local nenhum na memória
+const sobrenomeAluno = null
+
+// boolean = true ou false (valor lógico)
+const aprovado = true
+const boolean2 = false
+
+// 
