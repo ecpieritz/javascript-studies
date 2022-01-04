d619e332212ce1619433671abfca77a1c1a63916
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-01-04 10:43:33 -0300

    added lesson 105

---
+++ secao-01-basic-javascript/105-const/script.js
@@ -0,0 +1,35 @@
+// variável constante
+
+const nomeCompleto = 'Emilyn C. Pieritz'
+
+/* 
+REGRAS DAS CONSTANTES:
+- NÃO PODE MODIFICAR O VALOR DE UMA CONSTANTE
+- não tem como criar com palavras reservadas (ex: let, if, else...)
+- precisam ter nomes significativos, que dê para entender do que se trata
+		Exemplos do que pode: 
+			nome
+			idade
+			totalIdade
+		
+		Exemplos do que não pode:
+			n
+			x1
+			t
+- não pode começar o nome da constante com numero. Ex: 1nome
+- não pode conter espaço ou traços.
+		Exemplos:
+			nome-cliente
+			nome cliente
+- utilizar camelCase para constantes
+- case sensitive
+- não utilize var, utilize const
+*/
+
+const numero1 = 5
+const numero2 = 10
+const resultado = numero1 * numero2
+console.log(resultado)
+
+// como ver o tipo da variável
+console.log(typeof numero2)
