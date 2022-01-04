f3718f6591988e089eed1ccefcfdcd7a0494db0b
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-01-04 10:54:12 -0300

    added lesson 106

---
+++ secao-01-basic-javascript/106-exercicio-const-e-let/script.js
@@ -0,0 +1,8 @@
+const nome = 'Emilyn Pieritz'
+const idade = 30
+const peso = 103
+const alturaEmMetro = 1.6
+let imc
+imc = peso / (alturaEmMetro * alturaEmMetro)
+
+console.log(`${nome} tem ${idade} anos e pesa ${peso}kg. Tem ${alturaEmMetro}m de altura e seu IMC é de ${imc}`)
