8a6dcfdd340481d820a78f925a7de24c3c7ad4f4
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-01-05 10:02:31 -0300

    Added lesson 109

---
+++ secao-01-basic-javascript/109-alert-confirm-prompt/js/script.js
@@ -0,0 +1,10 @@
+/*
+Confirm retorna valor booleano
+confirm('deseja realmente apagar?')
+
+Alerta retorna undefined
+alert('Mensagem do alerta!')
+
+Prompt sempre retorna string
+prompt('Digite seu nome:')
+*/
