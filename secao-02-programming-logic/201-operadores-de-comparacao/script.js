bc738665217896091dc4d65f3caea7916a8df13a
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-01-11 17:54:56 -0300

    Added lesson 201

---
+++ secao-02-programming-logic/201-operadores-de-comparacao/script.js
@@ -0,0 +1,12 @@
+/*
+Operadores de comparação:
+  > maior que
+  >= maior ou igual a
+  < menor que
+  <= maior ou igual a
+  == igualdade (checa valor)
+  === igualdade estrita (checa valor e tipo)
+  != diferente (checa valor)
+  !== diferente estrito (checa valor e tipo)
+
+*/
