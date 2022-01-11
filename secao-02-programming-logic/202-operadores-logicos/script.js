b994ac40d8b018f838ba8df428772f70c15d789e
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-01-11 18:16:38 -0300

    Added lesson 202

---
+++ secao-02-programming-logic/202-operadores-logicos/script.js
@@ -0,0 +1,10 @@
+/*
+Operadores lógicos:
+  && and(e)
+  || or(ou)
+  ! not(não)
+*/
+
+const expAnd = true && true && true && false//precisa que todos sejam verdadeiros para retornar verdadeiro
+const expOr = true || false//se 1 deles retorna verdadeiro, a expressão é verdadeira
+const expNot = !false//estou negando a informação, no caso o false é verdadeiro e se fosse !true seria false
