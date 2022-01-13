2e94f24cf1d3ec489c6c53488596002ed1bee12f
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-01-13 11:31:25 -0300

    Added lesson 204 - part 2

--- secao-02-programming-logic/204-if-elseif-else/script.js
+++ secao-02-programming-logic/204-if-elseif-else/script.js
@@ -24,3 +24,21 @@ if(tenhoDinheiro){
 }else{
   console.log('não vou ir a padaria')
 }
+
+console.log('----Parte 2----')
+
+// os blocos de IF e ELSE dependem um do outro
+const num = 10
+// se isso for true, faça isso
+if(num >= 0 && num <= 5){
+  console.log('O numero está entre 0 e 5')
+} else if (num >= 6 && num <= 8){
+  console.log('O numero está entre 6 e 8')
+} else if (1 === 1){ //se tiver uma condição TRUE fora da lógica e que anteceda a expressão que seria TRUE, ela executará e quebrará o código
+  console.log('LITERAL')
+} else if (num >= 9 && num <= 11){
+  console.log('O numero está entre 9 e 11')
+} else{
+  //se não for true, faça isso
+  console.log('O numero NÃO está entre 0 e 11')
+}
